9d390c2d30730d033644f97b317282aa61591167
LucasAliagaT <aliaga_18@hotmail.com>
2021-09-21 11:04:05 -0300

    armando estructura

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+   
 const express = require('express');
 const path = require('path');
 
-const app = express ();
+const app = express();
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
+})
+
+app.listen(3050, () => console.log('Server corriendo en el puerto 3000'));
