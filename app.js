559afb72d83246cc8a856ed8fbcfd689bddc9d6d
JeyEfPy <jfporta@gmail.com>
2021-09-26 11:16:28 -0300

    Creación path a views app.js

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 
+app.get ("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+
+app.get ("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
+
+app.get ("/carrito", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+})
+
+app.get ("/detalleProducto", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/detalleProducto.html"));
+})
+
 app.listen(3100, () => console.log('Server corriendo en el puerto 3100'));
