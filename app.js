00ae07c2f0527026af0962ee962adc97e5ab0859
LucasAliagaT <aliaga_18@hotmail.com>
2021-10-14 16:13:28 -0300

    EJS

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
