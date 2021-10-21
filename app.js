37cc60339a0ebf784082ec9d5ecb602bf0ae13a2
JeyEfPy <jfporta@gmail.com>
2021-10-21 20:37:40 -0300

    Carpetas de rutas

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/home.ejs'));
 })
 
 app.get ("/register", (req, res) => {
